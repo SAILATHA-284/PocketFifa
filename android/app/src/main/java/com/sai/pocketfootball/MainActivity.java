e09f39eff9106d57dee95053b94e77303c265ab9
Sailatha Shetty <sai.she.rt22@dypatil.edu>
2026-07-11 03:45:25 +0530

    apk

---
+++ android/app/src/main/java/com/sai/pocketfootball/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sai.pocketfootball;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sai.pocketfootball',
+  appName: 'Pocket Football',
+  webDir: 'dist'
+};
+
+export default config;
